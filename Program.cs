290eb55a57e9622aad7e7ed7fadbaa85497adb83
yifenghuang34 <85208482+yifenghuang34@users.noreply.github.com>
2021-07-16 12:17:46 +0800

    sanding email
    sand email to email

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MimeKit;
+using System;
+using System.Threading.Tasks;
+
+namespace Message2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IServiceCollection service = new ServiceCollection();
+
+            service.AddSingleton<IEmail, Email>();
+
+            var provider = service.BuildServiceProvider().GetRequiredService<IEmail>();
+
+            var message = new MimeMessage
+            {
+                Subject = "我是郵件主題",
+                Body = new BodyBuilder
+                {
+                    HtmlBody = $"測試測試，時間:{DateTime.Now:yyyy-MM-dd HH:mm:ss}"
+                }.ToMessageBody()
+            };
+
+            await provider.SendEmailAsync(message);
+        }
+    }
+}
